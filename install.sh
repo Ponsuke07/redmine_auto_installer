7b012cf150b91e01855a6b7dfbecfebb53eefc0e
Ponsuke07 <yamane07ynct@gmail.com>
2017-09-02 01:14:42 +0900

    fix error handling
    Previously, install.sh did not stop even if errors occured.
    This commit support the automatic exit from install.sh on error.

--- install.sh
+++ install.sh
@@ -52,149 +52,127 @@ function vecho() {
     return 0
 }
 
-vecho "Download Redmine..."
-wget "http://www.redmine.org/releases/redmine-${redmine_ver}.tar.gz" -O "${wwwroot}/redmine.tar.gz"
-
-if [ $? -eq 0 ]
-then
-    vecho "Redmine is downloaded."
-
-    vecho "Expand downloaded tar.gz..."
-    if [ $verbose_flg -eq 1 ]
-    then
-        tar zxvf "${wwwroot}/redmine.tar.gz" -C "${wwwroot}"
+function check_result() {
+    if [ "$?" -ne "0" ] ;then
+        echo "Error: Faild to ${1:-somewhere}." >&2
+        exit 1
     else
-        tar zxf "${wwwroot}/redmine.tar.gz" -C "${wwwroot}"
+        vecho "done."
     fi
-fi
+}
 
-if [ $? -eq 0 ]
+procName="Download Redmine"
+vecho "$procName..."
+dwnFile="${wwwroot}/redmine.tar.gz"
+wget "http://www.redmine.org/releases/redmine-${redmine_ver}.tar.gz" -O "${dwnFile}"
+check_result "$procName"
+
+
+procName="Expand downloaded tar.gz"
+vecho "$procName..."
+if [ "$verbose_flg" -eq 1 ]
 then
-    vecho "Expand is successfully done."
-
-    vecho "Rename redmine directory..."
-    mv ${wwwroot}/redmine-${redmine_ver}/ ${wwwroot}/${redminedir}/
+    tar zxvf "${wwwroot}/redmine.tar.gz" -C "${wwwroot}"
+else
+    tar zxf "${wwwroot}/redmine.tar.gz" -C "${wwwroot}"
 fi
+check_result "$procName"
 
-if [ $? -eq 0 ]
-then
-    vecho "Renamed."
 
-    vecho "Create database..."
-    mysql -u root --password="${password}" -e "create database if not exists redmine_${uname} character set utf8;"
-    if [ $? -eq 0 ]
-    then
-        mysql -u root --password="${password}" -e "grant all on redmine_${uname}.* to '${uname}'@'localhost' identified by '${upass}';"
-    fi
-fi
+procName="Rename redmine directory"
+vecho "$procName..."
+mv ${wwwroot}/redmine-${redmine_ver}/ ${wwwroot}/${redminedir}/
+check_result "$procName"
+procName="Create database"
+vecho "$procName..."
+mysql -u root --password="${password}" -e "create database if not exists redmine_${uname} character set utf8;"
+check_result "$procName"
+mysql -u root --password="${password}" -e "grant all on redmine_${uname}.* to '${uname}'@'localhost' identified by '${upass}';"
+check_result "$procName"
 
-if [ $? -eq 0 ]
-then
-    vecho "Database creation is done."
 
-    vecho "Config redmine database..."
-    vecho "Change directory into redmine..."
-    cd redmine
-    cat <<EOF > config/database.yml
+procName="Config redmine database"
+vecho "$procName..."
+vecho "Change directory into redmine"
+cd redmine
+cat <<EOF > config/database.yml
 production:
   adapter: mysql2
   database: redmine_${uname}
   host: ${host}
   usernae: ${uname}
 EOF
-    if [ ! $upass == "" ]
-    then
-        cat password: ${upass} >> config/database.yml
-    fi
-    cat <<EOF >> config/database.yml
+if [ ! $upass == "" ]
+then
+    cat password: ${upass} >> config/database.yml
+fi
+cat <<EOF >> config/database.yml
   encoding: utf8
 EOF
-fi
+check_result "$procName"
 
-if [ $? -eq 0 ]
-then
-    vecho "Configuration is done."
 
-    vecho "Install bundler from gem..."
-    gem install bundler
-fi
+procName="Install bundler from gem"
+vecho "$procName..."
+gem install bundler
+check_result "$procName"
 
-if [ $? -eq 0 ]
-then
-    vecho "Bundler is successfully installed."
 
-    vecho "Resolve dependency with bundler..."
-    bundle install --without development test postgresql sqlite --path vandor/bundle
-fi
+procName="Resolve dependency with bundler"
+vecho "$procName..."
+bundle install --without development test postgresql sqlite --path vandor/bundle
+check_result "$procName"
 
-if [ $? -eq 0 ]
-then
-    vecho "Dependency resolved."
 
-    vecho "Generate session-store secret key..."
-    bundle exec rake generate_secret_token
-fi
+procName="Generate session-store secret key"
+vecho "$procName..."
+bundle exec rake generate_secret_token
+check_result "$procName"
 
-if [ $? -eq 0 ]
-then
-    vecho "Key is generated."
 
-    vecho "Create table..."
-    RAILS_ENV=production bundle exec rake db:migrate
-fi
+procName="Create table"
+vecho "$procName..."
+RAILS_ENV=production bundle exec rake db:migrate
+check_result "$procName"
 
-if [ $? -eq 0 ]
-then
-    vecho "Table is created."
 
-    vecho "Sign in Default data..."
-    RAILS_ENV=production REDMINE_LANG=ja bundle exec rake redmine:load_default_data
-fi
+procName="Sign in Default data"
+vecho "$procName..."
+RAILS_ENV=production REDMINE_LANG=ja bundle exec rake redmine:load_default_data
+check_result "$procName"
 
-if [ $? -eq 0 ]
-then
-    vecho "Default data is loaded."
 
-    chown -R ${uname}:${uname} files log tmp public/plugin_assets
-    if [ $? -eq 0 ]
-    then
-        chmod -R 755 files log tmp public/plugin_assets
-    fi
-fi
+chown -R ${uname}:${uname} files log tmp public/plugin_assets
+check_result "$procName"
+chmod -R 755 files log tmp public/plugin_assets
+check_result "$procName"
 
-if [ $? -eq 0 ]
-then
-    vecho "Redmine Installation is done."
 
-    vecho "Install Passenger..."
-    gem install passenger --no-rdoc --no-ri
-    passenger-install-apache2-module --auto
-fi
+procName="Install Passenger"
+vecho "$procName..."
+gem install passenger --no-rdoc --no-ri
+passenger-install-apache2-module --auto
+check_result "$procName"
 
-if [ $? -eq 0 ]
-then
-    vecho "Passenger installation is done."
 
-    vecho "Setting apache..."
-    cat <<EOF > /etc/httpd/conf.d/redmine.conf
+procName="Setting apache"
+vecho "$procName..."
+cat <<EOF > /etc/httpd/conf.d/redmine.conf
 <Directory "${wwwroot}/redmine/public">
 Require all granted
 </Directory>
 EOF
-    bash -c "passenger-install-apache2-module --snippet >> /etc/httpd/conf.d/redmine.conf"
-fi
+bash -c "passenger-install-apache2-module --snippet >> /etc/httpd/conf.d/redmine.conf"
+check_result "$procName"
 
-if [ $? -eq 0 ]
-then
-    vecho "Apache setting is done."
+chown -R apache:apache ${wwwroot}/redmine
+ln -s ${wwwroot}/redmine/public ${wwwroot}/${accessdir}
+bash -c "echo 'RackBaseURI /${accessdir}' >> -a /etc/httpd/conf.d/redmine.conf"
 
-    chown -R apache:apache ${wwwroot}/redmine
-    ln -s ${wwwroot}/redmine/public ${wwwroot}/${accessdir}
-    bash -c "echo 'RackBaseURI /${accessdir}' >> -a /etc/httpd/conf.d/redmine.conf"
-    service httpd configtest
-fi
+procName="apache configtest"
+vecho "$procName..."
+service httpd configtest
+check_result "$procName"
 
-if [ $? -eq 0 ]
-then
-    service httpd graceful
-fi
+service httpd graceful
+
